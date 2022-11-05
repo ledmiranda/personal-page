18cdbd2f6259c01fe042d3abba6e14b46abc8d20
Luis Miranda <luismiranda@MacBook-Pro-de-Luis.local>
2022-11-05 09:51:16 -0500

    Include Navbar menu and redirect to different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import About from './pages/About';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
